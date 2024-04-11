77400637ac860f922db78a29df36e2aa6629bef1
joaoheinke <joaovtito@gmail.com>
2024-04-11 09:11:54 -0300

    dois primeiros exercicios

--- client.js
+++ client.js
@@ -1,4 +1,39 @@
 const axios = require("axios");
+
+const config = {
+    headers : {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    }
+}
+
 axios
-  .get("https://catfact.ninja/fact")
-  .then((response) => console.log(response.data.fact));
+    .post("https://tecweb-js.insper-comp.com.br/token", { username: "joaovhs1" }, config)
+    .then((response) => {
+        token = response.data.accessToken;
+        console.log(token);
+
+        const config2 = {
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+        }
+        axios
+            .get("https://tecweb-js.insper-comp.com.br/exercicio", config2)
+            .then((response) => console.log(response))
+
+    });
+
+
+
+// console.log(token);
+
+function soma(a, b){
+  return a + b;
+}
+
+function tamanhoDaString(str) {
+  return str.length;
+}
